42f243e374d97761d5c6803cfad78a63a8827880
Charles Jackson (HEADSTORM) <charles.jackson@climate.com>
2021-12-03 14:31:22 -0600

    02 - Solve

--- 02-dive.lisp
+++ 02-dive.lisp
@@ -6,6 +6,9 @@
     (down integer)
     (up integer))
 
+  (define-type (position :d :h :a)
+    (position :d :h :a))
+
   (define input
     (pipe
      (lisp (list (list string)) ()
@@ -13,13 +16,34 @@
        (cl:mapcar
          (cl:lambda (line)
            (cl-list-to-coalton (str:split " " line :omit-nulls cl:t)))
-         (str:split #\newline (uiop:read-file-string "input/02-practice") :omit-nulls cl:t))))
+         (str:split #\newline (uiop:read-file-string "input/02") :omit-nulls cl:t))))
      (map (fn (line)
             (match line
-              ((cons "forward" (cons x xs))
-               (forward (fromsome "" (parse-int x))))
-              ((cons "down" (cons x xs))
-               (down (fromsome "" (parse-int x))))
-              ((cons "up" (cons x xs))
-               (up (fromsome ""  (parse-int x)))))))))
+              ((cons "forward" (cons x _))
+               (forward (fromsome-parse-int x)))
+              ((cons "down" (cons x _))
+               (down (fromsome-parse-int x)))
+              ((cons "up" (cons x _))
+               (up (fromsome-parse-int x))))))))
+
+  (define (execute-instructions current-position instructions)
+    (let ((final-position
+            (fold
+             (fn (instruction current-position)
+               (match current-position
+                 ((position depth horizontal aim)
+                  (match instruction
+                    ((forward x)
+                     (position (+ (* x aim)
+                                  depth)
+                               (+ horizontal x) aim))
+                    ((down x)
+                     (position depth horizontal (+ aim x)))
+                    ((up x)
+                     (position depth horizontal (- aim x)))))))
+             current-position
+             instructions)))
+      (match final-position
+        ((position depth horizontal _)
+         (* depth horizontal)))))
   )
