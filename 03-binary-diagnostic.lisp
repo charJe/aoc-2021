900c536a3eca21dea5ddeb11dfab88345a93aca6
Charles <charles.b.jackson@protonmail.com>
2021-12-04 01:22:56 -0600

    03 - Solve part 2

--- 03-binary-diagnostic.lisp
+++ 03-binary-diagnostic.lisp
@@ -35,4 +35,60 @@
                  gamma)))
       (* (binary-to-decimal gamma)
          (binary-to-decimal epsilon))))
+
+  (define part2-input
+    (pipe
+     input
+     (map
+      (fn (row)
+        (map (fn (num)
+               (if (== -1 num) 0 1))
+             row)))))
+
+  (define (rating decider i input)
+    (match (tail input)
+      ((none) (make-list 0))
+       ;; only one element left
+      ((some (nil)) (fromsome "no head" (head input)))
+      ((some _)
+       (let ((decision
+               (pipe
+                input
+                (map (fn (row)
+                       (match (index row i)
+                         ((none) -1)
+                         ((some x) x))))
+                decider)))
+         (pipe
+          input
+          (filter (fn (row)
+                    (pipe
+                     row
+                     (flip index i)
+                     (fromsome "index out of range")
+                     (== decision))))
+          (rating decider (+ i 1)))))))
+
+  (define (decider comparison nums)
+    (match (pipe
+            nums
+            (filter (/= -1))
+            (partition (== 0)))
+      ((tuple zeros ones)
+       (if (comparison
+            (length zeros)
+            (length ones))
+           1 0))))
+
+  (define (oxygen-rating input)
+    (binary-to-decimal
+     (rating (decider <=) 0 input)))
+
+  (define (scrubber-rating input)
+    (binary-to-decimal
+     (rating (decider >) 0 input)))
+
+  (define (life-support input)
+    (* (oxygen-rating input)
+       (scrubber-rating input)))
   )
