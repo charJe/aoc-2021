2c0f83b67cbe7f7cbe4fbe7669dda53a552caf6c
Charles <charles.b.jackson@protonmail.com>
2021-12-04 12:07:16 -0600

    Add split-string utility

--- util.lisp
+++ util.lisp
@@ -8,6 +8,12 @@
                   (uiop:read-file-string filename)
                   :omit-nulls cl:t))))
 
+  (define (split-string d s)
+    (lisp (list string) (d s)
+      (cl-list-to-coalton
+       (str:split d s :omit-nulls cl:t))))
+
   (define (fromsome-parse-int x)
     (fromsome (concat-string x " is not an integer")
-              (parse-int x))))
+              (parse-int x)))
+  )
