8c7d84fb143b18ff3b263ed0197e5daa4c7681a1
Charles <charles.b.jackson@protonmail.com>
2021-12-03 23:51:55 -0600

    Add file reading utility

--- util.lisp
+++ util.lisp
@@ -1,6 +1,13 @@
 (cl:in-package #:advent-of-code)
 
 (coalton-toplevel
+  (define (read-lines filename)
+    (lisp (list string) (filename)
+      (cl-list-to-coalton
+       (str:split #\newline
+                  (uiop:read-file-string filename)
+                  :omit-nulls cl:t))))
+
   (define (fromsome-parse-int x)
     (fromsome (concat-string x " is not an integer")
               (parse-int x))))
