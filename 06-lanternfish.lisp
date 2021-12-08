d1d2b308325accb462627dfc36e91fe6f930b3c4
Charles <charles.b.jackson@protonmail.com>
2021-12-07 18:42:20 -0600

    06 - Solve

---
+++ 06-lanternfish.lisp
@@ -0,0 +1,28 @@
+(cl:in-package #:advent-of-code)
+
+(coalton-toplevel
+  (define birthing 0)
+  (define mother 6)
+  (define baby 8)
+
+  (define input
+    (pipe
+     "input/06"
+     read-lines head (fromsome "no input")
+     (split-string ",")
+     (map fromsome-parse-int)
+     (fold
+      (fn (age fishs)
+          (replace-by (+ 1) age fishs))
+      (repeat (+ baby 1) 0))))
+
+  (define (simulate days fishs)
+   (if (== 0 days)
+       (sum fishs)
+       (simulate
+        (- days 1)
+        (let ((babies (fromsome "no head" (head fishs))))
+          (replace-by (+ babies) mother
+                      (append (drop 1 fishs)
+                              (make-list babies)))))))
+)
