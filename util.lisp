587913e23dff148aef4c59943a518a69ce4bb6b6
Charles <charles.b.jackson@protonmail.com>
2021-12-12 23:58:48 -0600

    Remove split-string
    in preference to the build in split

--- util.lisp
+++ util.lisp
@@ -38,10 +38,6 @@
                   (uiop:read-file-string filename)
                   :omit-nulls cl:t)))
 
-  (define (split-string d s)
-    (lisp (list string) (d s)
-       (str:split d s :omit-nulls cl:t)))
-
   (define (fromsome-parse-int x)
     (fromsome (concat-string x " is not an integer")
               (parse-int x)))
